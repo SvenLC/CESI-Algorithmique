54779e50ea316c629bb7126624e1374fd4c6781d
SvenLC <sven.lecann@live.fr>
2019-10-15 17:26:26 +0200

    refacto

--- listes/triParSelection.py
+++ listes/triParSelection.py
@@ -1,9 +1,10 @@
+# Ecrire la procédure qui reçoit un tableau de taille N en entrée et qui réalise un tri par sélection
 
-exemple = [1,3,5,6,3,2,4,5,6]
+exemple = [1, 3, 5, 6, 3, 2, 4, 5, 6]
 
 
-def triParSelection(tableau, n):    
-    for i in range(0, n):        
+def triParSelection(tableau, n):
+    for i in range(0, n):
         for j in range(i, n):
             if(tableau[i] > tableau[j]):
                 min = tableau[j]
@@ -11,8 +12,5 @@ def triParSelection(tableau, n):
                 tableau[i] = min
     return tableau
 
+
 print(triParSelection(exemple, 9))
-
-
-
-
